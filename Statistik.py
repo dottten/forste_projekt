4bc56b03840714beb2a4834eed9a8be50fbbf04f
Lasse Bruun <Lasse_bruun@hotmail.com>
2018-06-13 12:09:44 +0200

    Tilføjet beskrivelse af statistikkerne

--- Statistik.py
+++ Statistik.py
@@ -15,53 +15,60 @@ import numpy as np
 # første kolonner til udregning 
 # Statistic er den ønskede statistik, der er 7 muligheder
 
-# data = np.array([[12, 4, 5], [15, 2, 9], [18, 2, 8], [20, 4, 10], [22, 3, 12]])
+data = np.array([[12, 4, 5], [15, 2, 9], [18, 2, 8], [20, 4, 10], [22, 3, 12]])
 def dataStatistics(data, statistic):
     # Middelværdi for temperaturen
     if statistic == "Mean Temperature":
         MnTmp = np.mean(data[:,0])
         result = ("The {:s} is {:.3f} degrees".format(statistic, MnTmp))
+        print("You have chosen to see the average of the valid temperatures")
         
         
     # Middelværdi for vækstraten
     elif statistic == "Mean Growth rate":
         MnGrw = np.mean(data[:,1])
         result = ("The {:s} is {:.3f}".format(statistic, MnGrw))
+        print("You have chosen to see the average of the valid growth rates")
         
         
     # Standard afvigelsen af temperaturen
     elif statistic == "Std Temperature":
         StdTmp = np.std(data[:,0])
         result = ("The {:s} is {:.3f}".format(statistic, StdTmp))
+        print("You have chosen to see the standard deviation of the valid temperatures")
         
         
     # Standard afvigelsen af vækstraten
     elif statistic == "Std Growth rate":
         StdGrw = np.std(data[:,1])
         result = ("The {:s} is {:.3f}".format(statistic, StdGrw))
+        print("You have chosen to see the standard deviation of the valid growth rates")
         
         
     # Antallet af rækker
     elif statistic == "Rows":
         Rows = np.size(data, axis = 0)
         result = ("There are {:.0f} {:s}".format(Rows, statistic))
+        print("You have chosen to see the number of rows in the data set")
         
         
     # Middelværdi for vækstraten for temperaturer under 20 grader
     elif statistic == "Mean Cold Growth rate":
         Cold = np.mean(data[:,1][data[:,0] < 20])
         result = ("The {:s} is {:.3f}".format(statistic, Cold)) 
-  
+        print("You have chosen to see the average growth rate for temperatures under 20 degrees")
+        
     
     # Middelværdi for vækstraten for temperaturer over 50 grader
     elif statistic == "Mean Hot Growth rate":
         Hot = np.mean(data[:,1][data[:,0] > 50])
         result = ("The {:s} is {:.3f}".format(statistic, Hot))
+        print("You have chosen to see the average growth rate for temperatures over 50 degrees")
         
         
     # Hvis der er en fejl
     else:
-        result = ("Error, please try again")
+        result = ("Error, not a valid option. Please try again")
    
     return result
-#print(dataStatistics(data, "Rows"))
+print(dataStatistics(data, "Mean Temperature"))
